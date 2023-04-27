e32799af57ee4b2f15f76e3f5abe05cb71a2bd73
shdam <soren.helweg@gmail.com>
2023-04-27 13:39:46 +0200

    ensure factors in pdata

--- inst/script/dmap.R
+++ inst/script/dmap.R
@@ -7,6 +7,7 @@ exprs_dmap$X <- NULL
 
 rownames(pData_dmap) <- pData_dmap$X
 pData_dmap$X <- NULL
+pData_dmap$cell_types <- as.factor(pData_dmap$cell_types)
 
 usethis::use_data(exprs_dmap, overwrite = TRUE)
 usethis::use_data(pData_dmap, overwrite = TRUE)
--- inst/script/ilaria.R
+++ inst/script/ilaria.R
@@ -1,7 +1,5 @@
 ## code to prepare `ilaria` dataset goes here
 
-library(spaceRAT)
-
 exprs_ilaria <- read.csv(system.file("extdata/exprs_ilaria.csv", package = "spaceRATScaffolds"))
 pData_ilaria <- read.csv(system.file("extdata/pData_ilaria.csv", package = "spaceRATScaffolds"))
 
@@ -10,6 +8,8 @@ exprs_ilaria$X <- NULL
 
 rownames(pData_ilaria) <- pData_ilaria$X
 pData_ilaria$X <- NULL
+pData_ilaria$cancer_type <- as.factor(pData_ilaria$cancer_type)
+
 
 usethis::use_data(exprs_ilaria, overwrite = TRUE)
 usethis::use_data(pData_ilaria, overwrite = TRUE)
